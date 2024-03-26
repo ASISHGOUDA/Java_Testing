4638f8704470fd9a269072b0e531b861f4e59814
Asishkumar <gouda0682@gmail.com>
2024-03-26 13:15:06 +0530

    Initial commit-1

---
+++ src/app.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+public class app {
+  public static void main(String[] args) {
+      Map<Integer, String> basicMap = new HashMap<>();
+       basicMap.put(1, "hello");
+      basicMap.put(2, "hello");
+      basicMap.put(3, "hello");
+      basicMap.put(4, "hello");
+
+      System.out.println(basicMap);
+  }
+
+}
